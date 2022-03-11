7933801b429fcb00412468b8ec3ee7cada70bb1c
Leonida <nyaataleonida@gmail.com>
2022-03-11 22:57:37 +0300

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/*
+ * main -entry point
+ *
+ * return: o if successful, non-zero otherwise
+ */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
