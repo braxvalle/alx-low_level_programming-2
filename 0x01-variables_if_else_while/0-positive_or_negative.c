3be700b26ca31cfeecae75739e1b67d0f0aebd70
Leonida <nyaataleonida@gmail.com>
2022-03-13 00:20:56 +0300

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,6 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
-* betty style doc for function main goes there
 */
 
 int main(void)
@@ -14,6 +13,7 @@ int main(void)
 
 			srand(time(0));
 			n = rand() - RAND_MAX / 2;
+
 				if (n == 0)
 					printf("%d is zero\n", n);
 				else if (n < 0)
