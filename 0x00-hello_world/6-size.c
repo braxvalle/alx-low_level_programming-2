3373b681c5ef21f7d81438d6bb4b6a52bb93a331
Leonida <nyaataleonida@gmail.com>
2022-03-13 12:05:19 +0300

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)");
+	printf("Size of an int: 4 byte(s)");
+	printf("Size of a long int: 4 byte(s)");
+	printf("Size of a long long int: 8 byte(s)");
+	printf("Size of a float: 4 byte(s)");
+	return (0);
+	}
