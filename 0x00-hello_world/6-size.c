b7b3b79cb153b901d2dbd6f43fabf66ca2463aa9
Leonida <nyaataleonida@gmail.com>
2022-03-13 12:24:39 +0300

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,13 @@
  */
 int main(void)
 {
+	printf("size32\n");
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
+	printf("size64\n");
 	printf("Size of a char: 1 byte(s)\n");
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
