83f029e1f1af838e2e75edb73f9ace93ad539a57
Leonida <nyaataleonida@gmail.com>
2022-03-13 12:26:50 +0300

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 bye(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
 	return (0);
 	}
