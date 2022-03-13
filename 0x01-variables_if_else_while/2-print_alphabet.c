859f0f313618ffcdfc019c5bc6ffbb20a7af7c4d
Leonida <nyaataleonida@gmail.com>
2022-03-13 16:35:23 +0300

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	Char ch;
+	for ( ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+ 
