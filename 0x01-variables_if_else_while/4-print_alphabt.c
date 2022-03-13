3435a9d9378434e789b8983729f9931361a48f2f
Leonida <nyaataleonida@gmail.com>
2022-03-13 18:18:42 +0300

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,8 @@ int main(void)
 {
 	char ch;
 
-	for ((ch = 'a'; ch <= 'z'; ch++) && (ch != 'e'; && ch != 'q';))
+	for ((ch = 'a'; ch <= 'z') && (ch != 'e'; && ch != 'q'))
+		ch++;
 		putchar(ch);
 	putchar ('\n');
 	return (0);
