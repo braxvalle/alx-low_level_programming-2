1cf228cfcf8a9a33ec0e0758dc90ce5041f6499d
Leonida <nyaataleonida@gmail.com>
2022-03-13 23:06:29 +0300

    variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 15; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
