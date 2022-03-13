f763e483c8ac140da63488539501a2bc327f9f73
Leonida <nyaataleonida@gmail.com>
2022-03-13 18:09:55 +0300

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for ((ch = 'a'; ch <= 'z'; ch++) && (ch != 'e'; && ch != 'q'))
+		putchar(ch);
+	putchar ('\n');
+	return (0);
+}
