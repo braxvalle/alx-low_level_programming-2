5b2aa537a9d03ac1bed9e860c88a7235ca7e1b63
Leonida <nyaataleonida@gmail.com>
2022-03-13 17:21:46 +0300

    variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	char ch;
 
-	for(ch = 'A'; ch <= 'Z'; ch++)
-		for(ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			putchar(ch);
 	putchar ('\n');
 	return (0);
