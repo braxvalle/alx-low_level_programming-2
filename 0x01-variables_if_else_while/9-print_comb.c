e2fcf98c63b4f90a447ed7dde8bc1394623bfcb4
Leonida <nyaataleonida@gmail.com>
2022-03-14 23:56:01 +0300

    variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,16 @@ int main(void)
         int j;
 
         for (j = '0'; j <= '9'; j++)
+	{
                 putchar(j + '0');
         if (j < 9)
-                putchar(',');
+	{
+        putchar(',');
 	putchar(' ');
+	}
+	}
         putchar('\n');
+
         return (0);
 }
 
