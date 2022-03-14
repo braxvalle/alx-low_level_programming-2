261bcbd7694e162042ef68aa5bbc33af96f72405
Leonida <nyaataleonida@gmail.com>
2022-03-14 23:48:31 +0300

    variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+        int j;
+
+        for (j = '0'; j <= '9'; j++)
+                putchar(j + '0');
+        for (j < 9)
+                putchar(',');
+	putchar(' ');
+        putchar('\n');
+        return (0);
+}
+
