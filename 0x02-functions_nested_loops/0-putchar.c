c8428bd0c0f7772253103b1a7d5130e569cec932
Leonida <nyaataleonida@gmail.com>
2022-03-15 17:35:55 +0300

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+;
 /**
  * main - prints _putchar
  * Return: always 0 (Success)
  */
 int main(void)
 {
+	char ch;
 	putchar(_putchar);
 	putchar('\n');
 	return (0);
