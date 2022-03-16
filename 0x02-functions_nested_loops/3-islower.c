2d18ff72aa4429c00bfdc7eaae3eaafb24740512
Leonida <nyaataleonida@gmail.com>
2022-03-16 23:30:19 +0300

    nested

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
