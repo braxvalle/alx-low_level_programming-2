18dcec252f568386c59e6383e438e04674ad5c27
Leonida <nyaataleonida@gmail.com>
2022-03-22 01:26:23 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev - outputs a string in reverse
+ * @str: represents the string
+ * Return: returns nothing
+ */
+void _print_rev(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0' j++)
+	{
+	}
+	for (j = j - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
