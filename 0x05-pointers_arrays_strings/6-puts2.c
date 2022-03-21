6995d000217564036b73ff52ff5cf3ce7d22638f
Leonida <nyaataleonida@gmail.com>
2022-03-22 02:09:32 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *puts2 - prints every character
+ *@str: represents the character
+ *Return: returns nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *c = str;
+	int m;
+
+	while (*c != '\0')
+	{
+		c++;
+		i++;
+	}
+	j = i - 1;
+	for (m = 0 ; m <= l ; m++)
+	{
+		if (m % 2 == 0)
+		{
+			_putchar(str[m]);
+		}
+	}
+	_putchar('\n');
+}
