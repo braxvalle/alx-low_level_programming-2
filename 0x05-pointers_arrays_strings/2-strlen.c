f6d766e552c736b43a5a7bfc0ccce948291fffb1
Leonida <nyaataleonida@gmail.com>
2022-03-22 00:46:57 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _strlen(char *s) - returns the lenth of a string
+ * @s; represents the string
+ * Return; always 0
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(*s);
+	return (0);
+}
+
