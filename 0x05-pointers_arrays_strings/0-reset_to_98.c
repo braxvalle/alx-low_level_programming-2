679abd36f581ed5a7fb000a9b5ce9047579be20e
Leonida <nyaataleonida@gmail.com>
2022-03-22 00:10:50 +0300

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,12 +9,10 @@
 int main(void)
 {
     int n;
-    int *p;
 
     n = 402;
-    p = &n;
-    /*printf("n=%d\n", n);*/
-    /*reset_to_98(&n);*/
+    printf("n=%d\n", n);
+    reset_to_98(&n);
     *p = 98;
     printf("n=%d\n", n);
     return (0);
