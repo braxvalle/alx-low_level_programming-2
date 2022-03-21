d4349e8d34d58ea05beed6f1df5c55763adfb682
Leonida <nyaataleonida@gmail.com>
2022-03-22 01:12:34 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - returns a string
+ * @str: represents the string
+ * Return: returns nothing
+ */
+void _puts(char *str)
+        {
+	int j = 0;
+
+	while (*(str + j) != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
+
