41c91315497f870010cae95924727c7f16712566
Leonida <nyaataleonida@gmail.com>
2022-03-24 17:53:08 +0300

    more pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,12 @@
  */
 char *string_toupper(char *n)
 {
-	int j
+	int j = 0;
 
-	j = 0;
-	while (n[j]!= '\0')
+	while (n[j] != '\0')
 	{
-		if (n[j]>= 'a' && n[j]<= 'z')
-			n[j]= n[j]- 32;
+		if (n[j] >= 'a' && n[j] <= 'z')
+			n[j] = n[j]- 32;
 		j++
 	}
 	return (n);
