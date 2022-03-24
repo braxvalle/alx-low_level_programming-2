22ccf4ab72425bb2807d2ef388282399e4ff8670
Leonida <nyaataleonida@gmail.com>
2022-03-24 16:48:15 +0300

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,8 @@ char *_strncat(char *dest, char *src, int n)
 	dest_size = 0;
 
 	while (dest[a++])
-	{
 		dest_size++;
-	}
+
 	for (a = 0; src[a] && a < n; a++)
 		dest[dest_size++] = src[a];
 	return (dest);
