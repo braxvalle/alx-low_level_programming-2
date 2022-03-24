8b5c605f530b3f93145334ac32125eca3980d2fa
Leonida <nyaataleonida@gmail.com>
2022-03-24 17:14:31 +0300

    more pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	
+	if (i > j)
+	{
+		dest = dest[i];
+	}
+	else if (i < j)
+	{
+		dest = dest[j];
+	}
+	else 
+		dest = 0;
+	return (dest);
+}
