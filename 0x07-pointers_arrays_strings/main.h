81d68fb22fec059ff0af93dc80a584ecc895fe6b
Leonida <nyaataleonida@gmail.com>
2022-03-28 19:48:52 +0300

    more pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 void _putchar (char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
