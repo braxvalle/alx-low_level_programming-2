c7f03496446912818a4835be2e3ea342ec2940c5
Leonida <nyaataleonida@gmail.com>
2022-03-28 22:51:28 +0300

    more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,8 +13,6 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-	{
 		memory[index] = value;
-	}
-	return(memory);
+	retur ((memory);
 }
