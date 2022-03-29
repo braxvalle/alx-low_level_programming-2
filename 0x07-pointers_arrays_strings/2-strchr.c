320a828811f453479768e8494b1384d115bf97f5
Leonida <nyaataleonida@gmail.com>
2022-03-29 14:07:02 +0300

    more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: a character
+ */
+void *_strchr(char *s, char c)
+{
+	char ch;
+	int j;
+
+	for (s[j] = 0; s[j] != '\0'; j++)
+	{
+
+		if (s[j] == ch)
+		{
+			s[j] = c;
+		}
+		else
+		{
+			s[j] = NULL;
+		}
+	}
+	return (s);
+}
+
