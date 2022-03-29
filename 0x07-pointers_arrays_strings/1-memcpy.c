4b3b6ce665f9122f68012770df862094ee6d1c2a
Leonida <nyaataleonida@gmail.com>
2022-03-29 13:30:34 +0300

    more pointers

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@ void *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j = 0;
 
-	while (j < n)
+	while (j <= n)
 	{
 		dest[j] = src[j];
 		j++;
 	}
-	return (*dest);
+	return (dest);
 }
