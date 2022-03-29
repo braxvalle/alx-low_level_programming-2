a75f20e86f74e07e2c46002912d95d92cc0aef58
Leonida <nyaataleonida@gmail.com>
2022-03-29 12:50:18 +0300

    more pointers

---
+++ 0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory
+ * @s: pointer
+ * @b: constant byte
+ * @n: an unsigned integer
+ * Return: returns pointer
+ */
+void *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	j = 0;
+	while (j < n)
+	{
+		s[j] = b;
+	}
+	j++;
+	return (s);
+}
