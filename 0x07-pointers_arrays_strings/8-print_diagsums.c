6af4164d098a6fbfead8456cff58a753a26d8223
Leonida <nyaataleonida@gmail.com>
2022-03-29 22:34:32 +0300

    more pointers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints sum of 2 diagnonals
  * @a: pointer
@@ -6,7 +7,7 @@
  * Return: returns nothing
  */
 void print_diagsums(int *a, int size)
-{
+	{
 	int index, sum1 = 0, sum2 = 0;
 
 	for (index = 0; index < size; index++)
