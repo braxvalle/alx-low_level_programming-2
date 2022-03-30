61e6ba6144e4ca57061b20db9ca34f926e60bf99
Leonida <nyaataleonida@gmail.com>
2022-03-30 12:35:59 +0300

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,29 @@
 #include "main.h"
 /**
+ * sqtRecursive - calculates the squareroot recursively
+ * @start: inital number
+ * @end: last number
+ * @m: an integer value
+ * Return: gives the squareroot
+ **/
+int sqtRecursive(int start, int end, int m)
+{
+	long mid;
+
+	if (end >= start)
+	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == m)
+			return (mid);
+
+		if (mid * mid > m)
+			return (sqtRecursive(start, mid - 1, m));
+		if (mid * mid < m)
+			return (sqtRecursive(mid + 1, end, m));
+	}
+	return (-1);
+}
+/**
  * _sqrt_recursion - finds the natural squareroot of a number
  * @n: an integer value
  * Return: squareroot of number
@@ -10,5 +34,5 @@
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt_recursion(2, n, n));
+	return (sqtRecursive(2, n, n));
 }
