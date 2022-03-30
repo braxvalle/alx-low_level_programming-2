a5c0daae92a6a80f04590eb7aef0781dc2989241
Leonida <nyaataleonida@gmail.com>
2022-03-30 12:44:09 +0300

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number - finds a prime number
+ * @n: an integer value
+ * Return: returns a prime number
+ */
+int is_prime_number(int n)
+{
+	if (n / n == 0 && n / 1 == n)
+		return (1);
+	else
+		return (0);
+}
