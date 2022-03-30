56af1a3be25e4ca3e4a5b97880215c2238a7517c
Leonida <nyaataleonida@gmail.com>
2022-03-30 12:07:13 +0300

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
 void _putchar (char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,5 +6,3 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-
-#endif
