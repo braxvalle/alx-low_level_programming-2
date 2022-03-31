5f24adacdbe53676aea7aa0bd406ee75eeb320ee
Leonida <nyaataleonida@gmail.com>
2022-03-31 18:14:36 +0300

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,39 @@
 #include "main.h"
 /**
- * is_prime_number - finds a prime number
+ * sqtRecursive - calculates the squareroot recursively
+ * @n: an integer
+ * @m: comparison number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtRecursive(int n, int m)
+{
+	if (n <= 0)
+		return (-1);
+	if (n * n == m)
+		return (n);
+	return (sqtRecursive(n - 1, m));
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: an integer
+ * Return: returns the squareroot
+ **/
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	return (sqtRecursive(n / 2, n));
+}
+/**
+ * is_prime_number - checks if a number is prime
  * @n: an integer value
  * Return: returns a prime number
- */
+ **/
 int is_prime_number(int n)
 {
-	if (n / n == 0 && n / 1 == n)
-		return (1);
-	else
+	if (n <= 1 || _sqrt_recursion(n) >= 1)
 		return (0);
+	if (_sqrt_recursion(n) == -1)
+		return (1);
+	return (_sqrt_recursion(n));
 }
