c83097e35773d951a57d89244074d9afc3c623ca
Leonida <nyaataleonida@gmail.com>
2022-04-01 22:16:44 +0300

    arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,11 +7,8 @@
  */
 int main(int argc, char **argv)
 {
-	int i;
+	argv = argv;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("argc[%d]: %s\n". i, argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
