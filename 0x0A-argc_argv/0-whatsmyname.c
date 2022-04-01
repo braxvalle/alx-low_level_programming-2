d252f989baeb487fcd20bb423828e7c474b2bd6d
Leonida <nyaataleonida@gmail.com>
2022-04-01 21:55:29 +0300

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: character string
  * Return: always 0 (success)
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
