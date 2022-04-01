b81b47e81842ed8436a093a052dd030ab8a87c8b
Leonida <nyaataleonida@gmail.com>
2022-04-01 21:51:38 +0300

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * @argc: integer
+ * @argv: character string
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv);
+	return (0);
+}
