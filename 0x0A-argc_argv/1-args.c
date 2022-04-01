dcca9c8b837084bc4936f3c737b6873d8f5b4528
Leonida <nyaataleonida@gmail.com>
2022-04-01 22:10:59 +0300

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument number
+ * @argv: argument strings
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
